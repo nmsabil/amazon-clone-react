218301664bc2c06d26949293f8ccb64082dddd1f
Sabil <sabilmdtv1@gmail.com>
2020-09-15 20:02:34 +0100

    add to basket funciton to dispatch the item to the basket

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "../css/Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className='product'>
       <div className='product__info'>
@@ -19,7 +33,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} />
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
