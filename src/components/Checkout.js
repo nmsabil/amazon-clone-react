b00f24e305eebb91f81b1c23babb402a58b39e55
Sabil <sabilmdtv1@gmail.com>
2020-09-17 19:19:15 +0100

    added remove from cart function that updates the state and deletes perticular index

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,7 +6,7 @@ import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket }, dispatch] = useStateValue();
-  console.log(basket);
+
   return (
     <div className='checkout'>
       <div className='checkout__left'>
