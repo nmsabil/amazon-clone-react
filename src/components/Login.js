e044f3ad842d93d934fd4d98da4327d57ccfdb37
Sabil <sabilmdtv1@gmail.com>
2020-09-18 22:22:55 +0100

    login and register functionality added using firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,33 @@
+import { auth } from "../firebase";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "../css/Login.css";
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signin = (e) => {
     e.preventDefault();
-    // firebase login authentication
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
   const register = (e) => {
     e.preventDefault();
-    // firebase register, push user to database
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
   };
   return (
     <div className='login'>
